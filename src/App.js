8f44bd836b9983b0bb39b9b1e29ab78ec4bca168
McCarthy Family <mccarthyfamily@McCarthys-iMac.local>
2018-11-02 14:17:07 -0400

    changed app text

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ class App extends Component {
           <p>
             Sure Everybody!  Edit <code>src/App.js</code> and save to reload.
           </p>
-          <p>Very cool</p>
+          <p>Very cool, this code was originally changed on feature</p>
           <a
             className="App-link"
             href="https://reactjs.org"
